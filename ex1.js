2f070d20b604532469317c826b742be050a732a1
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-18 14:54:37 +0200

    done-ex1

--- ex1.js
+++ ex1.js
@@ -8,23 +8,33 @@
 /**
  * Cambia el título de la página a rojo usando un estilo en línea
  */
+document.querySelector("h1").style.color = "red";
 
 /**
  * Cambia el tamaño del título de la página a 48px usando un estilo en línea
  */
 
+document.querySelector("h1").style.fontSize = "48px";
+
 /**
  * Muestra por console.log el texto de la caja 2
  */
+let intro = document.querySelector("#caja-2").textContent;
+console.log(intro);
 
 /**
  * Cambia el color del borde del párrafo p.intro a verde usando un estilo en línea
  */
 
+document.querySelector(".intro").style.borderColor = "green";
+
 /**
  * Elimina la clase "estilo-3" de la última caja.
  * Ayuda: https://www.w3schools.com/howto/howto_js_remove_class.asp
  */
+
+document.getElementById("ultimo").classList.remove("estilo-3");
+
 
 /**
  * Añade la clase CSS "ocultar" al penúltimo párrafo
@@ -33,3 +43,5 @@
  * BONUS: Haz el ejercicio sin modificar el ex1.html
  
  */
+
+document.querySelector("p:nth-of-type(4)").classList.add("ocultar");
